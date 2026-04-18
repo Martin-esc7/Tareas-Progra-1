b147648fddf74633c64517ef783134a4bc44399e
Martin-esc7 <mescobaro5@miumg.edu.gt>
2026-04-17 21:00:44 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
-#include <iostream> 
-#include <winsock2.h> 
-int main(){ 
-WSADATA w; WSAStartup(MAKEWORD(2,2),&w); 
-SOCKET s=socket(AF_INET,SOCK_STREAM,0); 
-struct sockaddr_in sv; sv.sin_family=AF_INET; sv.sin_port=htons(3306); sv.sin_addr.s_addr=inet_addr("127.0.0.1"); 
-if(connect(s,(struct sockaddr*)&sv,sizeof(sv))==0) std::cout<<"Conexion exitosa con MySQL!"<<std::endl; 
-return 0;} 
+#include <iostream>
+#include <winsock2.h>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2,2), &wsaData);
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(3306);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) == 0) {
+        cout << "Conexion exitosa con MySQL!" << endl;
+    } else {
+        cout << "Error al conectar" << endl;
+    }
+
+    closesocket(sock);
+    WSACleanup();
+    return 0;
+}
